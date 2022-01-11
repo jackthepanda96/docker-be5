f02c8930e22c1f3889d322df1346b491c0b9604b
Jerry Young <jerryyoung2911@gmail.com>
2022-01-11 16:16:29 +0700

    Update config

--- configs/config.go
+++ configs/config.go
@@ -37,24 +37,24 @@ func initConfig() *AppConfig {
 	var defaultConfig AppConfig
 	defaultConfig.Port = 8000
 	defaultConfig.Database.Driver = "mysql"
-	defaultConfig.Database.Name = "be5db"
-	defaultConfig.Database.Address = "localhost"
+	defaultConfig.Database.Name = "defal"
+	defaultConfig.Database.Address = "dbbe5.c7svlamncepc.ap-southeast-1.rds.amazonaws.com"
 	defaultConfig.Database.Port = 3306
-	defaultConfig.Database.Username = "root"
-	defaultConfig.Database.Password = ""
+	defaultConfig.Database.Username = "admin"
+	defaultConfig.Database.Password = "admin123"
 
 	viper.SetConfigType("yaml")
 	viper.SetConfigName("config")
 	viper.AddConfigPath("./layered/configs/")
 	if err := viper.ReadInConfig(); err != nil {
-		log.Info("failed to open file")
+		log.Warn("failed to open file")
 		return &defaultConfig
 	}
 
 	var finalConfig AppConfig
 	err := viper.Unmarshal(&finalConfig)
 	if err != nil {
-		log.Info("failed to extract external config, use default value")
+		log.Warn("failed to extract external config, use default value")
 		return &defaultConfig
 	}
 	return &finalConfig
