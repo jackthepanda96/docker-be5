c729bc9d8638905c305b921ce957a4e8ca25879a
Jerry Young <jerryyoung2911@gmail.com>
2022-01-12 00:28:30 +0700

    Read from env

--- configs/config.go
+++ configs/config.go
@@ -1,22 +1,32 @@
 package configs
 
 import (
+	"fmt"
 	"sync"
 
 	"github.com/labstack/gommon/log"
 	"github.com/spf13/viper"
 )
 
+// type AppConfig struct {
+// 	Port     int `yaml:"port"`
+// 	Database struct {
+// 		Driver   string `yaml:"driver"`
+// 		Name     string `yaml:"name"`
+// 		Address  string `yaml:"address"`
+// 		Port     int    `yaml:"port"`
+// 		Username string `yaml:"username"`
+// 		Password string `yaml:"password"`
+// 	}
+// }
 type AppConfig struct {
-	Port     int `yaml:"port"`
-	Database struct {
-		Driver   string `yaml:"driver"`
-		Name     string `yaml:"name"`
-		Address  string `yaml:"address"`
-		Port     int    `yaml:"port"`
-		Username string `yaml:"username"`
-		Password string `yaml:"password"`
-	}
+	Port     int
+	Driver   string
+	Name     string
+	Address  string
+	DB_Port  int
+	Username string
+	Password string
 }
 
 var lock = &sync.Mutex{}
@@ -35,19 +45,28 @@ func GetConfig() *AppConfig {
 
 func initConfig() *AppConfig {
 	var defaultConfig AppConfig
+	// defaultConfig.Port = 8000
+	// defaultConfig.Database.Driver = ""
+	// defaultConfig.Database.Name = ""
+	// defaultConfig.Database.Address = ""
+	// defaultConfig.Database.Port = 3306
+	// defaultConfig.Database.Username = ""
+	// defaultConfig.Database.Password = ""
 	defaultConfig.Port = 8000
-	defaultConfig.Database.Driver = ""
-	defaultConfig.Database.Name = ""
-	defaultConfig.Database.Address = ""
-	defaultConfig.Database.Port = 3306
-	defaultConfig.Database.Username = ""
-	defaultConfig.Database.Password = ""
+	defaultConfig.Driver = ""
+	defaultConfig.Name = ""
+	defaultConfig.Address = ""
+	defaultConfig.DB_Port = 3306
+	defaultConfig.Username = ""
+	defaultConfig.Password = ""
 
-	viper.SetConfigType("yaml")
+	viper.SetConfigType("env")
 	viper.SetConfigName("config")
 	viper.AddConfigPath("./configs/")
+	// viper.SetConfigFile("config.env")
+
 	if err := viper.ReadInConfig(); err != nil {
-		log.Warn("failed to open file")
+		log.Warn(err)
 		return &defaultConfig
 	}
 
@@ -57,5 +76,6 @@ func initConfig() *AppConfig {
 		log.Warn("failed to extract external config, use default value")
 		return &defaultConfig
 	}
+	fmt.Println(finalConfig)
 	return &finalConfig
 }
--- utils/mysqldriver.go
+++ utils/mysqldriver.go
@@ -13,11 +13,16 @@ import (
 func InitDB(config *configs.AppConfig) *gorm.DB {
 
 	connectionString := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8&parseTime=True&loc=Local",
-		config.Database.Username,
-		config.Database.Password,
-		config.Database.Address,
-		config.Database.Port,
-		config.Database.Name,
+		config.Username,
+		config.Password,
+		config.Address,
+		config.DB_Port,
+		config.Name,
+		// config.Database.Username,
+		// config.Database.Password,
+		// config.Database.Address,
+		// config.Database.Port,
+		// config.Database.Name,
 	)
 	// "root:@tcp(127.0.0.1:3306)/be5db?charset=utf8&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(connectionString), &gorm.Config{})
